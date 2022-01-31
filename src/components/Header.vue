e1796e6c6c81ecdc9db4394b43ea3f768a59cc65
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2022-01-31 16:15:39 +0100

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <img src="../assets/img/spotify-logo.png"  style="height: 50px">
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+header{
+    background-color: #2E3A46;
+}
+</style>
